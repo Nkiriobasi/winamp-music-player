3496da7231359f5a6b71354928c392d9a161233f
Nkiriobasi <obasinkiri@gmail.com>
2022-11-28 16:15:01 +0100

    added the user-profile data to display the the ui

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './UserProfile.scss'
 // import { CgProfile } from 'react-icons'
-// import { MdArrowDropDown, MdArrowDropUp } from 'react-icons'
+import { MdArrowDropDown, MdArrowDropUp } from 'react-icons/md'
 
 
-const UserProfile = ({handleLogout, profile}) => {
+const UserProfile = ({handleLogout, userProfile}) => {
+  const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
 
   return (
-    <div className='user__profile'>
-        <img src="" alt="profile" />
-        <h3 className="user__profile-name">{}</h3>
+    <div className="user__profile" onClick={handleToggle} onBlur={handleToggle}>
+      <img src={userProfile.images[0].url} alt="profile" className='user__proile-img' />
+      <h4 className="user__profile-name">{userProfile.display_name}</h4>
+      {isActive ? <MdArrowDropDown className="arrowDown" /> : <MdArrowDropUp className="arrowDown" />}
 
-        <div className="user__profile-dropdown">
-            <h3 className="user__profile-email">{}</h3>
-            <h3 className="user__profile-country">{}</h3>
-            <h3 className='user__profile-log' onClick={handleLogout}>Log out</h3>
-        </div>
+      <div className={`user__profile-dropdown ${isActive ? "hide" : "show"}`}>
+        <h3 className="user__profile-pro">Profile</h3>
+        <h3 className='user__profile-log' onClick={handleLogout}>Log out</h3>
+      </div>
     </div>
   )
 }
