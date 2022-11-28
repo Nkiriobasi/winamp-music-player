519b89f103e88be741e1efdd6026613d7634c3f1
Nkiriobasi <obasinkiri@gmail.com>
2022-11-28 16:17:08 +0100

    exported Userprofile component as default

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as RenderArtists } from './RenderArtists/RenderArtists'
 export { default as UserProfile} from './UserProfile/UserProfile'
+export { default as NavHeader } from './NavHeader/NavHeader'
