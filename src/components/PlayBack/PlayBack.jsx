2336d64065373292413e9b7720ca58dcaeeafd60
Nkiriobasi <obasinkiri@gmail.com>
2022-11-29 12:56:44 +0100

    created a playback component

---
+++ src/components/PlayBack/PlayBack.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './PlayBack.scss'
+
+
+const PlayBack = () => {
+  return (
+    <section className='playback'>
+        <div className=""></div>
+    </section>
+  )
+}
+
+export default PlayBack
