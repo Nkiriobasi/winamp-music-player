cc579fb507fdabc0d89751a3691ba47d9e691576
Nkiriobasi <obasinkiri@gmail.com>
2022-11-29 13:01:39 +0100

    exported playback component as default from its root dirctory

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as RenderArtists } from './RenderArtists/RenderArtists'
 export { default as UserProfile} from './UserProfile/UserProfile'
 export { default as NavHeader } from './NavHeader/NavHeader'
+export { default as PlayBack } from './PlayBack/PlayBack'
