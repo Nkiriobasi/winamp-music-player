3f251f53f2574e91b35a1bf17a7342081803403b
Nkiriobasi <obasinkiri@gmail.com>
2022-12-01 10:49:17 +0100

    changed the import statement for importing spotify image

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { loginEndpoint } from '../../spotify'
 import './Login.scss'
+import { Images } from '../../static'
+
 
 
 const Login = () => {
   return (
     <div className='login'>
       <img 
-        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" 
+        src={Images.spotifyLogo} 
         alt="logo-spotify" 
         className='login__img'
       />
