12f57f2d33ce4191802660de84ce21bf6e332878
Nkiriobasi <obasinkiri@gmail.com>
2022-12-01 10:35:21 +0100

    imported spotify image from assets directory

--- src/static/Images.js
+++ src/static/Images.js
@@ -1,11 +1,13 @@
 
 import WinampRedLogo from '../assets/winamp-red-logo.png'
 import DefaultImg from '../assets/default-img.jpg'
+import SpotifyLogoWhite from '../assets/Spotify_Logo_RGB_White.png'
 
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     WinampRedLogo,
-    DefaultImg
+    DefaultImg,
+    SpotifyLogoWhite
 }
