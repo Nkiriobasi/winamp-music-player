50ef6cc7af507761a8bec5dd0b681415262873ff
Nkiriobasi <obasinkiri@gmail.com>
2022-12-06 15:43:08 +0100

    created a spinner component

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ClipLoader from 'react-spinners/ClipLoader';
+
+
+const Spinner = () => {
+  return (
+    <div style={{ width: '100px', margin: 'auto', display: 'block' }}>
+      <ClipLoader color="#52bfd9" size={100}/>
+    </div>
+  )
+}
+
+export default Spinner
