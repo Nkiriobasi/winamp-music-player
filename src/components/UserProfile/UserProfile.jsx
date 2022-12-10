5bd6adacb8f108ab317792799f0a14b5335cfebb
Nkiriobasi <obasinkiri@gmail.com>
2022-12-10 18:21:07 +0100

    updated the home page UI styles

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -16,10 +16,10 @@ const UserProfile = ({handleLogout, image, displayName}) => {
     <div className="user__profile" onClick={handleToggle} onBlur={handleToggle}>
       <img src={image} alt="profile" className='user__proile-img' />
       <h4 className="user__profile-name">{displayName}</h4>
+      <h4 className="name__tooltip">{displayName}</h4>
       {isActive ? <MdArrowDropDown className="arrowDown" /> : <MdArrowDropUp className="arrowDown" />}
 
       <div className={`user__profile-dropdown ${isActive ? "hide" : "show"}`}>
-        <h3 className="user__profile-pro">Profile</h3>
         <h3 className='user__profile-log' onClick={handleLogout}>Log out</h3>
       </div>
     </div>
